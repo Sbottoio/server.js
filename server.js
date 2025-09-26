2bf6eab15a3d83224b8c2e09cd245b3ebde75477
Sbottoio <sbottosbotto@gmail.com>
2025-09-26 12:37:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const path = require('path');
+const Gun = require('gun');
 
+const app = express();
+const PORT = process.env.PORT || 8765;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(PORT, () => {
+  console.log(`🚀 Server attivo su http://localhost:${PORT}`);
+});
+
+Gun({ web: server });
