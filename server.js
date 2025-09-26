721db1546f6803fe13c9acf854230732b5366244
Sbottoio <sbottosbotto@gmail.com>
2025-09-26 21:59:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const Gun = require('gun');
@@ -5,10 +6,13 @@ const Gun = require('gun');
 const app = express();
 const PORT = process.env.PORT || 8765;
 
+// Serve i file statici (index.html, CSS, immagini, ecc.)
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Avvia il server web
 const server = app.listen(PORT, () => {
   console.log(`🚀 Server attivo su http://localhost:${PORT}`);
 });
 
+// Avvia il nodo GUN P2P
 Gun({ web: server });
